86b65415284df4d3d2b70d92d474b58b8dda405d
vedya12 <161727554+vedya12@users.noreply.github.com>
2024-03-25 13:02:09 +0530

    Spotify Clone

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Lets write JavaScript');
+
+async function main(){
+let a= await fetch("http://127.0.0.1:5500/songs/")
+let response = await a.text();
+console.log(response)
+}
+
+main()
